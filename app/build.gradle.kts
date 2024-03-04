e9df49aec7f5f577834d7b0568ecfa548c0900e0
Bartosz Suśniło <bartosz.susnilo@mobilespot.pl>
2024-03-04 21:05:14 +0100

    FM-2 Plugin kapt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.jetbrainsKotlinAndroid)
-    kotlin("kapt")
+    id(libs.plugins.kapt.get().pluginId)
     alias(libs.plugins.daggerHiltAndroid)
 }
 
