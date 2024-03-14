0fc47ad059a8c64fd796bcbfc6e60c24d5bef64e
Bartosz Suśniło <bartosz.susnilo@mobilespot.pl>
2024-03-14 22:03:54 +0100

    FM-7 Refactor DailyCard and DashboardScreen

---
+++ app/src/main/java/pl/mobilespot/futuremirror/screen/DailyCard.kt
@@ -0,0 +1,64 @@
+package pl.mobilespot.futuremirror.screen
+
+import android.icu.text.DateFormat
+import android.icu.util.Calendar
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.tooling.preview.PreviewParameterProvider
+import androidx.compose.ui.unit.dp
+import pl.mobilespot.futuremirror.ui.padding
+
+@Composable
+fun DailyCard(day: Int, isFutureDay: Boolean) {
+    Card {
+        Row(
+            modifier = Modifier.padding(MaterialTheme.padding.small),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            Text(text = "$day", color = getColorForFutureDay(isFutureDay))
+        }
+    }
+}
+
+@Preview
+@Composable
+fun FutureDailyCardPreview(@PreviewParameter(DayProvider::class) day: Int) {
+    DailyCard(day, false)
+}
+
+@Preview
+@Composable
+fun PastDailyCardPreview(@PreviewParameter(DayProvider::class, limit = 1) day: Int) {
+    DailyCard(day, true)
+}
+
+class DayProvider : PreviewParameterProvider<Int> {
+    override val values = sequenceOf(1, 31)
+}
+
+@Composable
+fun GetDate() {
+    val calendar = Calendar.getInstance().time
+    val dataFormat = DateFormat.getDateInstance(DateFormat.FULL).format(calendar)
+    Text(dataFormat)
+}
+
+fun isFutureDay(
+    day: Int
+) = day > Calendar.getInstance()
+    .get(Calendar.DAY_OF_MONTH)
+
+fun getColorForFutureDay(isFutureDay: Boolean) = if (isFutureDay) {
+    Color.Gray
+} else Color.Black
--- app/src/main/java/pl/mobilespot/futuremirror/screen/DashboardScreen.kt
+++ app/src/main/java/pl/mobilespot/futuremirror/screen/DashboardScreen.kt
@@ -1,24 +1,16 @@
 package pl.mobilespot.futuremirror.screen
 
-import android.icu.text.DateFormat
 import android.icu.util.Calendar
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.tooling.preview.PreviewParameter
-import androidx.compose.ui.tooling.preview.PreviewParameterProvider
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -43,41 +35,10 @@ fun DashboardScreen(viewModel: DashboardViewModel = hiltViewModel()) {
             verticalArrangement = Arrangement.spacedBy(MaterialTheme.padding.small)
         ) {
             items(days.count(), itemContent = { item ->
-                DailyCard(days[item])
+                DailyCard(days[item], isFutureDay(days[item]))
             })
         }
     }
-}
-
-@Composable
-private fun DailyCard(day: Int) {
-    Card {
-        Row(
-            modifier = Modifier.padding(MaterialTheme.padding.small),
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.spacedBy(16.dp)
-        ) {
-            val color: Color = getColorForDay(day)
-            Text(text = "$day", color = color)
-        }
-    }
-}
-
-@Preview
-@Composable
-fun DailyCardPreview(@PreviewParameter(DayProvider::class) day: Int) {
-    DailyCard(day)
-}
-
-class DayProvider : PreviewParameterProvider<Int> {
-    override val values = sequenceOf(1, 31)
-}
-
-@Composable
-fun GetDate() {
-    val calendar = Calendar.getInstance().time
-    val dataFormat = DateFormat.getDateInstance(DateFormat.FULL).format(calendar)
-    Text(dataFormat)
 }
 
 @Preview(showBackground = true)
@@ -87,11 +48,3 @@ private fun DashboardScreenPreview() {
         DashboardScreen()
     }
 }
-
-private fun getColorForDay(
-    day: Int
-) = if (day <= Calendar.getInstance()
-        .get(Calendar.DAY_OF_MONTH)
-) {
-    Color.Black
-} else Color.Gray
