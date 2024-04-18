887c01ce6c289ffbf95e0637a504c19595ad85d8
Bartosz Suśniło <bartosz.susnilo@mobilespot.pl>
2024-04-18 14:29:12 +0200

    FM-18 Init with prepopulate database

--- app/src/main/java/pl/mobilespot/futuremirror/core/di/AppModule.kt
+++ app/src/main/java/pl/mobilespot/futuremirror/core/di/AppModule.kt
@@ -2,17 +2,26 @@ package pl.mobilespot.futuremirror.core.di
 
 import android.app.Application
 import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
 import dagger.Binds
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 import pl.mobilespot.futuremirror.data.FMDatabase
 import pl.mobilespot.futuremirror.data.NameDayDao
 import pl.mobilespot.futuremirror.namedays.LocalDataSource
+import pl.mobilespot.futuremirror.namedays.NameDay
 import pl.mobilespot.futuremirror.namedays.NameDaysDataSource
 import pl.mobilespot.futuremirror.namedays.NameDaysRepository
 import pl.mobilespot.futuremirror.namedays.Repository
+import timber.log.Timber
+import javax.inject.Provider
 import javax.inject.Singleton
 
 @Module
@@ -40,13 +49,15 @@ object AppModule {
     @Provides
     @Singleton
     fun provideDatabase(
-        application: Application
+        application: Application,
+        provider: Provider<NameDayDao>
     ): FMDatabase {
         return Room.databaseBuilder(
             context = application,
             klass = FMDatabase::class.java,
             name = "db"
         )
+            .addCallback(AppDatabaseCallback(provider))
             .fallbackToDestructiveMigration()
             .build()
     }
@@ -56,5 +67,24 @@ object AppModule {
     fun provideLogDao(
         logFMDatabase: FMDatabase
     ): NameDayDao = logFMDatabase.nameDayDao()
+}
 
+
+class AppDatabaseCallback(private val provider: Provider<NameDayDao>) :
+    RoomDatabase.Callback() {
+
+    override fun onCreate(db: SupportSQLiteDatabase) {
+        super.onCreate(db)
+        prepopulateDatabase()
+    }
+
+    @OptIn(DelicateCoroutinesApi::class)
+    private fun prepopulateDatabase() {
+        GlobalScope.launch(Dispatchers.IO) {
+            Timber.d("Start prepopulate database")
+            provider.get().insert(NameDay("Bogdan"))
+            provider.get().insert(NameDay("Zenek"))
+            Timber.d("End prepopulate database")
+        }
+    }
 }
