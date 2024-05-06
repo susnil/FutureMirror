09a099e69381759ab48701c98f2f20691f18d9de
Bartosz Suśniło <bartosz.susnilo@mobilespot.pl>
2024-05-06 21:51:38 +0200

    FM-26 Use basic scaffold view with bottom navigation

---
+++ app/src/main/java/pl/mobilespot/futuremirror/presentation/navigation/BottomNavigation.kt
@@ -0,0 +1,71 @@
+package pl.mobilespot.futuremirror.presentation.navigation
+
+import android.content.res.Configuration
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import pl.mobilespot.futuremirror.R
+import pl.mobilespot.futuremirror.designsystem.ui.theme.FutureMirrorTheme
+
+@Composable
+fun BottomNavigation(
+    items: List<BottomNavigationItem>,
+    selectedItem: Int,
+    onItemClick: (Int) -> Unit
+) {
+    NavigationBar(
+        modifier = Modifier.fillMaxWidth(),
+        containerColor = MaterialTheme.colorScheme.background,
+        tonalElevation = 10.dp
+    ) {
+        items.forEachIndexed { index, item ->
+            NavigationBarItem(
+                selected = index == selectedItem,
+                onClick = { onItemClick(index) },
+                icon = {
+                    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                        Icon(
+                            painter = painterResource(id = item.icon),
+                            contentDescription = null,
+                            modifier = Modifier.size(20.dp),
+                        )
+                        Spacer(modifier = Modifier.height(6.dp))
+                        Text(text = item.text, style = MaterialTheme.typography.labelSmall)
+                    }
+                },
+            )
+        }
+    }
+}
+
+data class BottomNavigationItem(
+    @DrawableRes val icon: Int,
+    val text: String
+)
+
+@Preview
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+fun BottomNavigationPreview() {
+    FutureMirrorTheme {
+        BottomNavigation(items = listOf(
+            BottomNavigationItem(icon = R.drawable.ic_icon, text = "Home"),
+            BottomNavigationItem(icon = R.drawable.ic_icon, text = "Search"),
+            BottomNavigationItem(icon = R.drawable.ic_icon, text = "News"),
+        ), selectedItem = 0, onItemClick = {})
+    }
+}
--- app/src/main/java/pl/mobilespot/futuremirror/presentation/navigation/NavGraph.kt
+++ app/src/main/java/pl/mobilespot/futuremirror/presentation/navigation/NavGraph.kt
@@ -1,8 +1,7 @@
 package pl.mobilespot.futuremirror.presentation.navigation
 
-import androidx.compose.foundation.layout.Row
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
+import android.annotation.SuppressLint
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.stringResource
 import androidx.navigation.NavType
@@ -17,49 +16,58 @@ import pl.mobilespot.futuremirror.presentation.news.NewsScreen
 import pl.mobilespot.futuremirror.presentation.search.SearchRoute
 import pl.mobilespot.futuremirror.presentation.settings.SettingsRoute
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun NavGraph() {
     val navController = rememberNavController()
-
-    NavHost(navController = navController, startDestination = Route.DashboardScreen.route) {
-        composable(route = Route.DashboardScreen.route) { _ ->
-            DashboardRoute()
-        }
-        composable(route = Route.SearchScreen.route) { _ ->
-            SearchRoute()
-        }
-        composable(
-            route = Route.DayDetailsScreen.route,
-            arguments = listOf(navArgument(Route.DayDetailsScreen.DAY_ID_ARG) {
-                type = NavType.StringType
-                defaultValue = null
-                nullable = true
-            })
-        ) { _ ->
-            //todo handle arg
-            DayDetailsScreen()
-        }
-        composable(route = Route.NewsScreen.route) { _ ->
-            NewsScreen()
-        }
-        composable(route = Route.Settings.route) { _ ->
-            SettingsRoute()
-        }
-    }
-    Row {
-        listOf(
-            Route.Settings.route to R.string.settings,
-            Route.SearchScreen.route to R.string.search,
-            Route.DayDetailsScreen.route to R.string.day_details
-        ).forEach {
-            Button(onClick = { navController.navigate(it.first) }) {
-                Text(text = stringResource(id = it.second))
+    val bottomNavigationItems = listOf(
+        Route.DashboardScreen.route to R.string.dashboard,
+        Route.Settings.route to R.string.settings,
+        Route.SearchScreen.route to R.string.search,
+        Route.DayDetailsScreen.route to R.string.day_details
+    )
+    //todo remove
+//    Button(onClick = { navController.navigateToDayDetails("4") }) {
+//        Text(text = "Go to 4 day details")
+//    }
+    Scaffold(bottomBar = {
+        BottomNavigation(
+            items = bottomNavigationItems.map {
+                BottomNavigationItem(
+                    R.drawable.ic_icon,
+                    stringResource(id = it.second)
+                )
+            },
+            selectedItem = 0,//todo selectedItem
+            onItemClick = { index ->
+                navController.navigate(bottomNavigationItems[index].first)
+            }
+        )
+    }) {
+        NavHost(navController = navController, startDestination = Route.DashboardScreen.route) {
+            composable(route = Route.DashboardScreen.route) { _ ->
+                DashboardRoute()
+            }
+            composable(route = Route.SearchScreen.route) { _ ->
+                SearchRoute()
+            }
+            composable(
+                route = Route.DayDetailsScreen.route,
+                arguments = listOf(navArgument(Route.DayDetailsScreen.DAY_ID_ARG) {
+                    type = NavType.StringType
+                    defaultValue = null
+                    nullable = true
+                })
+            ) { _ ->
+                //todo handle arg
+                DayDetailsScreen()
+            }
+            composable(route = Route.NewsScreen.route) { _ ->
+                NewsScreen()
+            }
+            composable(route = Route.Settings.route) { _ ->
+                SettingsRoute()
             }
         }
-        //todo remove
-        Button(onClick = { navController.navigateToDayDetails("4") }) {
-            Text(text = "Go to 4 day details")
-        }
     }
-
 }
