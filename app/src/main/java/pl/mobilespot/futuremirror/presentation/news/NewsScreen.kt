084c09f9e5708ec7806f37051abdd5eab52aa933
Bartosz Suśniło <bartosz.susnilo@mobilespot.pl>
2024-05-10 17:09:01 +0200

    FM-27 Example of scroll list

--- app/src/main/java/pl/mobilespot/futuremirror/presentation/news/NewsScreen.kt
+++ app/src/main/java/pl/mobilespot/futuremirror/presentation/news/NewsScreen.kt
@@ -1,8 +1,49 @@
 package pl.mobilespot.futuremirror.presentation.news
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.layout.positionInRoot
+import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.launch
+import kotlin.math.roundToInt
 
 @Composable
 fun NewsScreen() {
-    TODO("Not yet implemented")
+    val scrollState = rememberScrollState()
+    val coroutineScope = rememberCoroutineScope()
+    var scrollToPosition  by remember { mutableFloatStateOf(0F) }
+
+    Column(Modifier.verticalScroll(scrollState)) {
+        repeat(50) {
+            Text(
+                text = "Pre Item $it",
+                modifier = Modifier
+                    .padding(16.dp)
+                    .clickable {
+                        coroutineScope.launch {
+                            scrollState.animateScrollTo(scrollToPosition.roundToInt())
+                        }
+                    }
+            )
+        }
+        Text("Found me!",
+            modifier = Modifier.onGloballyPositioned { coordinates ->
+                scrollToPosition = coordinates.positionInRoot().y
+            })
+        repeat(55) {
+            Text(text = "Post item $it")
+        }
+    }
 }
