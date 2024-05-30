2fcbe56117a18e89b35d8b65513f4300948deb75
Bartosz Suśniło <bartosz.susnilo@mobilespot.pl>
2024-05-30 18:33:19 +0200

    FM-34 Center horizontally search screen

--- app/src/main/java/pl/mobilespot/futuremirror/presentation/search/SearchScreen.kt
+++ app/src/main/java/pl/mobilespot/futuremirror/presentation/search/SearchScreen.kt
@@ -1,6 +1,7 @@
 package pl.mobilespot.futuremirror.presentation.search
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -8,6 +9,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.semantics.testTag
@@ -21,26 +23,33 @@ fun SearchScreen(
     dayMonths: List<NameDay> = emptyList(), textValue: String = "",
     onTextChanged: (String) -> Unit = {}
 ) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    )
+    {
 
-    var textFieldValue by remember {
-        mutableStateOf(TextFieldValue(text = textValue))
-    }
-
-    Column {
-        TextField(
-            value = textFieldValue,
-            onValueChange = { newValue ->
-                textFieldValue = newValue
-                onTextChanged(newValue.text)
-            },
-            modifier = Modifier.semantics { testTag = SEARCH_NAME }
-        )
-        Text(text = "Name days:")
-        dayMonths.forEach {
-            Text(text = "Name: ${it.name} day: ${it.day} month: ${it.month}")
+        var textFieldValue by remember {
+            mutableStateOf(TextFieldValue(text = textValue))
         }
-        if (dayMonths.isEmpty()) {
-            Text(text = "No result")
+
+        Column {
+            TextField(
+                value = textFieldValue,
+                onValueChange = { newValue ->
+                    textFieldValue = newValue
+                    onTextChanged(newValue.text)
+                },
+                modifier = Modifier.semantics { testTag = SEARCH_NAME }
+            )
+            Text(text = "Name days:")
+            dayMonths.forEach {
+                Text(text = "Name: ${it.name} day: ${it.day} month: ${it.month}")
+            }
+            if (dayMonths.isEmpty()) {
+                Text(text = "No result")
+            }
         }
     }
 }
